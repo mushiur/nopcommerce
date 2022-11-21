3e16bfd894222599195cd393e13feb0725cbac98
mushiur <rifat1521054@gmail.com>
2022-11-21 16:05:44 +0600

    register data updated

--- testData/registerData.py
+++ testData/registerData.py
@@ -1,23 +1,18 @@
-import secrets
+import random
+import string
 
 # first name and last name
-
-
 firstName = "Rifat"
 lastName = "Rahman"
 
 # date of birth : day, month, year
-date = "12"
+date = random.randint(1, 31)
 month = "February"
-year = "1998"
-
+year = random.randint(1900, 2000)
 
 # Email Field
-def email_generator():
-    return f"{secrets.token_hex(8)}@gmail.com"
+email = ''.join(random.choice(string.ascii_lowercase) for i in range(10)) + "@gmail.com"
 
-
-email = email_generator()
 # Company field
 company = "Red.Digital Limited"
 
